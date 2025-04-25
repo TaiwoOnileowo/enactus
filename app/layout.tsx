943aa91bf777feab85636b6e583f953642ee9ae3
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2025-04-25 16:07:07 +0100

    add seo details

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,46 @@
 import Navbar from "@/components/navigation/navbar";
 import type { Metadata } from "next";
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Enactus",
   description: "Empowering Communities Through Entrepreneurial Action",
+  alternates: {
+    canonical: {
+      title: "Enactus",
+      url: "https://enactuscu.com",
+    },
+  },
+  openGraph: {
+    title: "Enactus ",
+    description: "Empowering Communities Through Entrepreneurial Action",
+    images: [
+      {
+        url: "/thumbnail.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Enactus CU",
+      },
+    ],
+    url: "https://enactuscu.com",
+  },
+  icons: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "32x32",
+      url: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+  ],
 };
 
 export default function RootLayout({
